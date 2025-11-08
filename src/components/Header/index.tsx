7f65b03f7011c57d7566268eb8bf58868b726065
minoura <minoura.web@gmail.com>
2025-11-08 21:50:07 +0900

    link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "preact-router";
 
 
 export default function Header() {
   return (
     <header class="bg-white shadow p-4">
-      <h1 class="text-xl font-bold text-blue-600">勉強ログ</h1>
+      <h1 class="text-xl font-bold text-blue-600">
+        {/* @ts-ignore */}
+        <Link href="/">勉強ログ</Link>
+      </h1>
     </header>
   )
 }
