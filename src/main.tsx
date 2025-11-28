6d6787fb2989c499dc01d9937d8a0f3af2910325
mino <81564697+umeshio@users.noreply.github.com>
2025-11-28 14:20:29 +0900

    style整理

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import { render } from 'preact'
-import './index.css'
-import App from './app.tsx'
+import { render } from 'preact';
+import './reset.css';
+import './index.css';
+import App from './app.tsx';
 
-render(<App />, document.getElementById('app')!)
+render(<App />, document.getElementById('app')!);
