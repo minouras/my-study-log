817c69a6c4cbe10aace65d3cc61269510054318d
mino <81564697+umeshio@users.noreply.github.com>
2025-12-16 14:44:12 +0900

    記事追加

---
+++ src/pages/posts/251216.tsx
@@ -0,0 +1,24 @@
+import { type PostMeta } from '../../lib/type';
+import ReactMarkdown from 'react-markdown';
+import content from './251216.md?raw';
+import SingleBase from '../../components/base/SingleBase';
+import '../../components/style/markdown.css';
+
+export const meta: PostMeta = {
+  title: 'PHPでのモジュールファイル読み込み',
+  date: '2025.12.16',
+  tags: ['PHP', 'require_once', 'use', 'chatGPT'],
+};
+
+export default function post251216() {
+  return (
+    <div>
+      <SingleBase title={meta.title} date={meta.date} tags={meta.tags}></SingleBase>
+      <article>
+        <div class="markdown">
+          <ReactMarkdown>{content}</ReactMarkdown>
+        </div>
+      </article>
+    </div>
+  );
+}
